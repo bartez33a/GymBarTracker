5d235bfce3045ad0e8e17f672ff1c3adb8c75910
Bartek <bartez33a@gmail.com>
2022-10-07 18:44:19 +0200

    Added openCV library

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
 
+//openCV
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
 int main() {
 	std::cout << "GymBarTracker\n";
 
+	cv::Mat src;
+	// create window
+	const char* window_name = "okienko";
+	cv::namedWindow(window_name, cv::WINDOW_AUTOSIZE);
+	// file name and location
+	std::string filename = "test.jpg";
+	std::string location = "images/";
+	std::string path = (location + filename);
+	// read data from file
+	src = cv::imread(path, cv::IMREAD_COLOR);
 
-	getchar();
+	char key = 0;
+	while (key != 'q')
+	{
+		if (src.data != NULL)
+		cv::imshow(window_name, src);
+		key = cv::waitKey(20);
+	}
+
+	cv::destroyAllWindows();
+
+
 	return EXIT_SUCCESS;
 }
